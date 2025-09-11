52566274a9df34d51eea8bbad77ba519a5b46d84
김백민 <Bengmii@naver.com>
2025-09-11 11:19:32 +0900

    Continue 문 추가

--- 10_break_pass.py
+++ 10_break_pass.py
@@ -1,13 +1,24 @@
+# import time
+# running = True
+# cup = 0
+#
+# while running:
+#     cup += 1
+#     print(cup)
+#     time.sleep(0.5)
+#     if cup == 10:
+#         # running = False
+#         break
+#
+# print('while 문 종료')
 import time
-running = True
-cup = 0
 
-while running:
-    cup += 1
-    print(cup)
-    time.sleep(0.5)
-    if cup == 10:
-        # running = False
-        break
+for i in range(1,10):
+    time.sleep(0.1)
+    if i % 3 == 0:
+        continue
 
-print('while 문 종료')
+    print(i)
+
+
+
