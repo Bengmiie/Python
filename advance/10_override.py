34814785b604fc8c4136fff73ae723c6252e1ddb
김백민 <Bengmii@naver.com>
2025-09-16 15:43:44 +0900

    수정

---
+++ advance/10_override.py
@@ -0,0 +1,23 @@
+class Car:
+    def start(self):
+        print('시동이 걸린다')
+
+    def run(self):
+        print('차가 시속 50km 로 달린다')
+
+    def stop(self):
+        print('차가 멈춘다')
+
+class MyCar(Car):
+
+    turbo = False
+
+    def run(self): # 부모와 같은 메서드를 사용하면 override 로 인식된다
+        if self.turbo == True:
+            print('차가 시속 200km 로 달린다')
+        else:
+            super().run()
+mc = MyCar()
+mc.start()
+mc.run()
+mc.stop()
